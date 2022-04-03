f5d15de23d92d09243f1a0f863c1d7a5c3fc253c
Axel Cola <axelcola1@gmail.com>
2022-04-02 21:23:40 -0300

    fixing styles errors

--- src/App.js
+++ src/App.js
@@ -45,10 +45,10 @@ function App() {
           imagen="Marge"
           cargo="Recruiter"
           empresa="Axel labs"
-          testimonio="Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. 
+          testimonio='"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. 
           Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor 
           (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal 
-          manera que logró hacer un libro de textos especimen. "
+          manera que logró hacer un libro de textos especimen. "'
         />
       </div>
     </div>
